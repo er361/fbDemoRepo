09c06f7a937ec1afc7dc74a2a08ae6707e2321f8
erbol dzhaumytbayev <sf7kmmr@gmail.com>
2021-11-05 18:30:26 +0600

    proxy factory

--- app/Models/Proxy.php
+++ app/Models/Proxy.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use App\Models\Helpers\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Proxy extends Model
 {
-    use HasFactory, Uuid;
+    use HasFactory, Uuid, SoftDeletes;
 
     protected $fillable = [
         'team_id',
